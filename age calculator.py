325fff215e6061b2c20f8ebffaaa372645846af2
nyi13 <nyilinhtut1994@gmail.com>
2020-11-26 23:26:26 +0630

    version2

--- age calculator.py
+++ age calculator.py
@@ -1,3 +1,4 @@
+#my first age calculator application
 user_input = int(input("Please enter your birth date: "))
 import datetime
 
@@ -5,4 +6,4 @@ current_year = datetime.datetime.now()
 b = current_year.year
 a = user_input
 i = b - a
-print(i, "years")
+print (i, years)
